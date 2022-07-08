7fb7c70895e6f8b533f2167c85c0c6f133b4c998
btboay <yuzhuoandlb@foxmail.com>
2022-07-08 17:19:50 +0800

    xor

---
+++ xor/main.c
@@ -0,0 +1,7 @@
+#include "xor.h"
+
+int main(int argc, char **argv)
+{
+    xor();
+    return 0;
+}
---
+++ xor/xor/xor.h
@@ -0,0 +1,19 @@
+#ifndef XOR_H
+#define XOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <lumos.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void xor();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
